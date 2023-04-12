203f5fb2dc187529eea451e5b0aade895955c2ed
Barndude <bwu22@stevens.edu>
2023-04-12 18:02:45 -0400

    updated next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	env: {
+		ATLAS_URI:
+			"mongodb+srv://barndudeW:KCeXfmiWMrtRv7yi@cluster0.brsjygl.mongodb.net/kanban",
+	},
+};
 
 module.exports = nextConfig
